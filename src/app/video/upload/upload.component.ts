bbdc58ae86477336ba70fe78b228e75d80327ef9
Duran <duran@Durans-Air.localdomain>
2023-07-07 11:39:12 +0200

    manage uploads

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid'
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app'
+import { ClipService } from 'src/app/services/clip.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss']
 })
-export class UploadComponent {
+export class UploadComponent implements OnDestroy {
+  isDragOver = false
+  file: File | null = null
+  nextStep = false
+  showAlert = false
+  alertColor = 'blue'
+  alertMsg = "Please wait! Your clip is being uploaded."
+  inSubmittion = false
+  percentage = 0
+  showPercentage = false
+  user: firebase.User | null = null
+  task?: AngularFireUploadTask
 
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  })
+
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipService: ClipService,
+    private router: Router
+  ) {
+    this.auth.user.subscribe((user) => this.user = user)
+  }
+
+  ngOnDestroy(): void {
+    this.task?.cancel()
+  }
+
+  storeFile($event: Event) {
+    this.isDragOver = false
+    this.file = ($event as DragEvent).dataTransfer ?
+      ($event as DragEvent).dataTransfer?.files.item(0) ?? null : ($event.target as HTMLInputElement).files?.item(0) ?? null
+    if (!this.file || this.file.type !== "video/mp4") {
+      return
+    }
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
+    this.nextStep = true
+  }
+
+  uploadFile() {
+    this.uploadForm.disable()
+    this.showAlert = true
+    this.alertColor = 'blue'
+    this.alertMsg = "Please wait! Your clip is being uploaded."
+    this.inSubmittion = true
+    this.showPercentage = true
+
+    const clipFileName = uuid()
+    const clipPath = `clips/${clipFileName}.mp4`
+    this.task = this.storage.upload(clipPath, this.file)
+    const clipRef = this.storage.ref(clipPath)
+
+    this.task.percentageChanges().subscribe((progress) => {
+      this.percentage = progress as number / 100
+    })
+    this.task.snapshotChanges().pipe(
+      last(),
+      switchMap(() => clipRef.getDownloadURL())
+    ).subscribe({
+      next: async (url) => {
+        const clip = {
+          uid: this.user?.uid as string,
+          displayName: this.user?.displayName as string,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          url,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }
+
+        const clipDocRef = await this.clipService.createClip(clip)
+
+        this.alertColor = 'green'
+        this.alertMsg = 'Success'
+        this.showPercentage = false
+        setTimeout(() => {
+          this.router.navigate([
+            'clip', clipDocRef.id
+          ])
+        }, 1000)
+      },
+      error: () => {
+        this.alertMsg = 'Upload failed!'
+        this.alertColor = 'red'
+        this.showPercentage = false
+        this.uploadForm.enable()
+      }
+    })
+  }
 }
